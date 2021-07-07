5fa027c6d8e338ece27b9b989512dcc145cd2f8f
Ganesa Vijayakumar <ganeshnet.vgp@gmail.com>
2021-07-07 18:38:50 +0530

    - Added all the required profile files

---
+++ src/main/java/com/sovanm/profiles/ProfilesApplication.java
@@ -0,0 +1,23 @@
+package com.sovanm.profiles;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Autowired
+private DBConfiguration dbConfig;
+
+@SpringBootApplication
+public class ProfilesApplication {
+	public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ProfilesApplication.class);
+        app.run();
+	}
+
+	public void run(String... args) throws Exception {
+        System.out.println("driverClassName: " + dbConfig.getDriverClassName);
+        System.out.println("url: " + dbConfig.getUrl);
+    }
+
+}
